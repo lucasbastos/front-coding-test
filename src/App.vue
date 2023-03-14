2a804d64e6150782ba1572f3258f037d309c3f14
Lucas Bastos <lucasbastos@MacBook-Pro-de-Lucas.local>
2023-03-14 14:22:02 -0300

    feat: Add axios and api call

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,49 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios';
+import { ref } from 'vue';
+
+interface Country {
+  ID: string;
+  Country: string;
+  CountryCode: string;
+  Slug: string;
+  TotalConfirmed: number;
+  TotalDeaths: number;
+  FatalitiesRate: number;
+}
+
+const Countries = ref([] as Country[])
+
+axios.get('https://api.covid19api.com/summary')
+  .then((response) => {
+    response.data.Countries.map((country: Country) => {
+      let countryData = {
+        ID: country.ID,
+        Country: country.Country,
+        CountryCode: country.CountryCode,
+        Slug: country.Slug,
+        TotalConfirmed: country.TotalConfirmed,
+        TotalDeaths: country.TotalDeaths,
+        FatalitiesRate: (country.TotalDeaths / country.TotalConfirmed * 100)
+      }
+      Countries.value.push(countryData)
+    })
+  })
+  .catch((error) => {
+    console.log(error);
+  })
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <div class="country" v-for="(country, index) in Countries" :key="index">
+      <h1>{{ country.Country }}</h1>
+      <p>Total Confirmed: {{ country.TotalConfirmed.toLocaleString() }}</p>
+      <p>Total Deaths: {{ country.TotalDeaths.toLocaleString() }}</p>
+      <p>Fatalities Rate: {{ country.FatalitiesRate.toFixed(2) }}%</p>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
